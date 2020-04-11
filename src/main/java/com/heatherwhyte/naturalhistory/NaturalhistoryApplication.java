d059771edc85892f3edfc006d58b7cb5a6b154bc
hbwhyte <heather.b.whyte@gmail.com>
2020-04-11 14:11:43 +0100

    Init commit

---
+++ src/main/java/com/heatherwhyte/naturalhistory/NaturalhistoryApplication.java
@@ -0,0 +1,13 @@
+package com.heatherwhyte.naturalhistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaturalhistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaturalhistoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heatherwhyte/naturalhistory/NaturalhistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heatherwhyte.naturalhistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaturalhistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
